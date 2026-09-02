0a165112c9cc15547858442c235d6ca20f0f6cc0
SHR-Zidane <pd01wvj@eduvaud.ch>
2026-09-02 12:16:03 +0200

    feat(ex1 & ex2): Avancement exercice

---
+++ personnel/Esport/DataSeries/DataSeries.cs
@@ -0,0 +1,20 @@
+namespace DataSeries;
+
+public class DataSeries<T>
+{
+    private readonly IEnumerable<T> _data;
+    
+    private DataSeries(IEnumerable<T> data) => _data = data;
+    
+    public static DataSeries<T> From(IEnumerable<T> source) => new DataSeries<T>(source);
+    
+    public int Count => _data.Count();
+    public IEnumerable<T> Values => _data;
+    
+    public static DataSeries<T> FromCsv(string path, Func<string[], T> parser)
+    {
+        var Lines = File.ReadAllLines(path).Skip(1);
+        return new DataSeries<T>(Lines.Select(line => parser(line.Split(','))));
+    }
+
+}
---
+++ personnel/Esport/Esport/Cs2Match.cs
@@ -0,0 +1,26 @@
+namespace Esport;
+
+public class Cs2Match
+{
+    public string Player { get; }
+    public string Map { get; }
+    public string StartSide { get; }  // côté joué en 1re mi-temps (CT ou T)
+    public int Kills { get; }
+    public int Deaths { get; }
+    public int Assists { get; }
+    public int Mvps { get; }
+    public bool Won { get; }
+
+    public Cs2Match(string player, string map, string startSide, int kills,
+        int deaths, int assists, int mvps, bool won)
+    { 
+        Player = player;
+        Map = map;
+        StartSide = startSide;
+        Kills = kills;
+        Deaths = deaths;
+        Assists = assists;
+        Mvps = mvps;
+        Won = won;
+    }
+}
