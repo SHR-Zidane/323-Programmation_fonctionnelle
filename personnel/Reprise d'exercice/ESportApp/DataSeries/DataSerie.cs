2850ba4b24fd2a50a722a1e650230683593eecf1
SHR-Zidane <pd01wvj@eduvaud.ch>
2026-09-23 12:07:35 +0200

    feat(mme): Ajout de la fonction qui permet de faire le mme

---
+++ personnel/Reprise d'exercice/ESportApp/DataSeries/DataSerie.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataSeries
+{
+    public class DataSerie<T>
+    {
+        private readonly IEnumerable<T> _data;
+
+        private DataSerie(IEnumerable<T> data) => _data = data;
+
+        public static DataSerie<T> From(IEnumerable<T> source)
+        {
+            return new DataSerie<T>(source);
+        }
+
+        // Créer une datasérie de type générique à partir d'un fichier CSV.
+        // Comme le format du fichier est spécifique au type, on ne sait pas
+        // comment le parser. C'est l'appelant de la méthode qui doit nous donner
+        // le bon outil, sous la forme de la fonction `parser`.
+
+        public static DataSerie<T> FromCsv(string filename, Func<string[], T> parser)
+        {
+            List<T> data = new List<T>();
+            try
+            {
+                List<string> content = File.ReadAllLines(filename).ToList();
+                foreach (string line in content.Skip(1))
+                {
+                    string[] cols = line.Split(',');
+                    data.Add(parser(cols));
+                }
+            } catch (Exception e) {
+                Console.WriteLine($"Erreur d'ouverture du fichier {e.Message}");
+            }
+            return From(data);
+        }
+        
+        public double MME(Func<T, double> value)
+        {
+            return _data
+                .Select(value)
+                .Aggregate((mme, val) => (mme + val) / 2.0);
+        }
+        
+        public int Count => _data.Count();
+        public IEnumerable<T> Values => _data;
+
+        public override string ToString()
+        {
+            return $"DataSerie<{typeof(T).Name}>: {Count} points: {Environment.NewLine}{String.Join(Environment.NewLine,_data.Select(s => s).ToArray())}";
+        }
+    }
+}
